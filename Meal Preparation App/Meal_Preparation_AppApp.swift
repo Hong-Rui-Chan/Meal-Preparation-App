a7620bef8f62d43ce2f7c096821f172a9f03b2d5
Ruisotto <chanhongrui22@gmail.com>
2025-05-05 14:49:09 +1000

    Initial Commit

---
+++ Meal Preparation App/Meal_Preparation_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_Preparation_AppApp.swift
+//  Meal Preparation App
+//
+//  Created by Chan Hong Rui on 5/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct Meal_Preparation_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
